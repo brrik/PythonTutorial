717a9b8bcfb90115ae367f2abc340fbaa49c716e
brrik0677 <leo.luvs.footwork@gmail.com>
2024-11-30 18:24:08 +0900

    アップデート

--- pass_and_continue.py
+++ pass_and_continue.py
@@ -4,10 +4,20 @@ names = ["tanaka", "yamada", "onishi"]
 for name in names:
     if name == "onishi":
         pass
+    else:
+        print(name)
+    #onishiだったとき、
+    #ifの処理だけが飛ぶので
+    #ここのprintは実行される
     print("＊名前が選択されました")
 
 #continueのパターン
 for name in names:
     if name == "onishi":
         continue
+    else:
+        print(name)
+    #onishiだったとき、
+    #つぎのループに飛ぶので
+    #ここのprintは実行されない
     print("＊名前が選択されました")
